3460f703473c1ea7e83767d1b1626926a5f1b227
mina jouneau <jouneaumina@gmail.com>
2020-08-17 16:58:54 +0200

    add airplanecontroller

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -1,28 +1,24 @@
-class Airplane < ApplicationRecord
-  belongs_to :user
-end
+class AirplanesController < ApplicationController
 
-def index
+  def index
     @airplanes = Airplanes.all
   end
 
-
-    def show
+  def show
     @airplane = Airplane.find(params[:id])
     @booking = Booking.new
   end
 
-
-   def new
+  def new
     @airplane = Airplane.new
   end
 
-
   def create
-  @airplane = Airplane.new(airplane_params)
-  if @airplane.save
+    @airplane = Airplane.new(airplane_params)
+    if @airplane.save
       redirect_to airplane_path(@airplane)
     else
       render 'new'
     end
   end
+end
