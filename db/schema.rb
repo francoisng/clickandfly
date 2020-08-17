dd17e347ef0d5e2594fa5fc53c3a3ab56f2a614d
Stanislas Legrez <stanislas27@gmail.com>
2020-08-17 16:12:53 +0200

    last db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_134401) do
+ActiveRecord::Schema.define(version: 2020_08_17_141031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airplanes", force: :cascade do |t|
+    t.string "address"
+    t.string "model"
+    t.string "category"
+    t.integer "seat_capacity"
+    t.integer "range"
+    t.integer "daily_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_airplanes_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "airplane_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["airplane_id"], name: "index_bookings_on_airplane_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +52,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_134401) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "airplanes", "users"
+  add_foreign_key "bookings", "airplanes"
+  add_foreign_key "bookings", "users"
 end
