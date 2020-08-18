03a37361898a9e96685bd90a52c842142220dced
stanislas legrez <stanislas27@gmail.com>
2020-08-18 12:13:02 +0200

    added athentification + create method in booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+    def create
+        @airplane = Airplane.find(params(:airplane_id))
+        @booking = Booking.new(booking_params)
+        @booking.airplane = @booking
+        if @booking.save
+            redirect_to airplane_path(@airplane)
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:airplane_id, :user_id)
+    end
+    
+
 end
