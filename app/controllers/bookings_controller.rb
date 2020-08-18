5cd0ecd1e0fa8298758ee97f291990bb1d5a9750
mina jouneau <jouneaumina@gmail.com>
2020-08-18 14:34:56 +0200

    add bookingcontroller show et edit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,16 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params) # je crée une nouvelle variable + je lui donne les données de mon formulaire
+    @airplane = Airplane.find(params[:airplane_id]) # je recupère l'id de airplane dans les params de ma page
+    @booking.airplane_id = @airplane.id # je donne la donnée  recupérée en ligne 4
+    @booking.user_id = current_user.id #je recupère la dernière donnée pour créer mon instance booking
+    @booking.save #maintenant qu ej'ai toutre sles odnnées j'enregsire
+    redirect_to airplane_path(@airplane)
+  end
 
+  private
 
-
-
-
-
-
-
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :airplanes
+  resources :airplanes do
+  resources :bookings, only: [:create]
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :airplanes
+
 end
