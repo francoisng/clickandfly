64aa1f6b2d0f2e9129dba2af984480854c3eb11e
Francois Nguyen <fnguyen90@gmail.com>
2020-08-20 07:26:20 +0200

    add search bar with multi column search

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -2,4 +2,11 @@ class Airplane < ApplicationRecord
   has_many :bookings
   belongs_to :user
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_model_and_category,
+    against: [ :model, :category ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
